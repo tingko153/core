1177a205dcc2867a8aed73cf2f928d106c11bda6
ㄴㄴㄴ <>
2018-04-19 13:13:01 +0900

    0419 list

---
+++ sh/src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="a in listData" :key="a.no"><input type="checkbox" :checked="false" />{{a.name}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+const a = [{name: 'test1', no: 0}, {name: 'test2', no: 1}];
+
+export default {
+  name: 'List',
+  data() {
+      return {
+          listData: a
+      }
+  }
+}
+</script>
