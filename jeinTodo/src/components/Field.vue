540914c614717b7837b1768e93477efb7adc3579
Jen.jein <kienaikizna@gmail.com>
2018-05-03 13:07:44 +0900

    add/delete 추가

---
+++ jeinTodo/src/components/Field.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="add">
+    <input type="text" ref="addText" /> <button v-on:click="add">추가</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Field',
+    props: ['addItem'],
+    methods: {
+      add() {
+        this.addItem(this.$refs.addText.value);
+        this.$refs.addText.value = '';
+      }
+    }
+  }
+</script>
+
