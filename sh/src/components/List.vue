288a8681b7bb58b6c5c80628d9ad4d6c3b87efe7
ㄴㄴㄴ <>
2018-05-10 13:05:50 +0900

    수정하기

--- sh/src/components/List.vue
+++ sh/src/components/List.vue
@@ -2,7 +2,10 @@
   <div>
       <ul>
           <li v-for="(a, index) in listData" :key="a.no" :data-num='a.no'>
-              <input type="checkbox" :checked="false" />{{a.name}}
+              <input type="checkbox" :checked="false" v-model="a.complete" />
+                <span v-if="a.modify===false">{{a.name}}</span>
+                <input v-else type="text" v-model='a.name' @keyup.enter="modify(index)" />
+              <a href="#" @click='modify(index)'>수정</a>
               <a href="#" @click='deleted(index)'>삭제</a>
           </li>
       </ul>
@@ -13,7 +16,7 @@
 
 export default {
   name: 'List',
-  props: ['listData'],
+  props: ['listData', 'modify'],
   methods: {
     deleted(n) {
         this.listData.splice(n, 1);
